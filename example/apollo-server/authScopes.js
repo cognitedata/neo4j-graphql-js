51b9d51eee937ad174dd81dc856da9c5cd15c42b
Kristian Aurlien <aurlien@users.noreply.github.com>
2021-01-20 13:32:11 +0100

    Set config for Cognite fork (#1)
    * Set name and version for cognite
    * remove example secrets
    * Set cognitedata url as git repository

--- example/apollo-server/authScopes.js
+++ example/apollo-server/authScopes.js
@@ -3,13 +3,11 @@ import { ApolloServer } from 'apollo-server';
 import neo4j from 'neo4j-driver';
 
 // JWT
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJzY29wZXMiOlsicmVhZDp1c2VyIiwiY3JlYXRlOnVzZXIiXX0.jCidMhYKk_0s8aQpXojYwZYz00eIG9lD_DbeXRKj4vA
 
 // scopes
 //  "scopes": ["read:user", "create:user"]
 
 // JWT_SECRET
-// oqldBPU1yMXcrTwcha1a9PGi9RHlPVzQ
 
 const typeDefs = `
 type User {
